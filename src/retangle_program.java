7609ca216b1c7b3a3ae3444f5ab59d31a50b2ad1
nmtuyen <nguyenmanhtuyen.haui@gmail.com>
2021-05-05 14:09:11 +0700

    tinh dien tich hinh chu nhat

---
+++ src/retangle_program.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class retangle_program {
+    public static void main(String[] args) {
+        float width;
+        float height;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter width");
+        width = sc.nextFloat();
+        System.out.println("Enter height");
+        height = sc.nextFloat();
+        float area = width * height;
+        System.out.println("Diện tích hình chữ nhật là "+ area);
+    }
+}
